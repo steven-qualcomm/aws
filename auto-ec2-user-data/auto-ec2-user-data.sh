2440db1c2462062110ff0d0a0c4429b6fca68ab5
haiwenj <haiwenj@qualcomm.com>
2019-03-05 10:33:07 +0800

    new upload

--- auto-ec2-user-data/auto-ec2-user-data.sh
+++ auto-ec2-user-data/auto-ec2-user-data.sh
@@ -1,4 +1,4 @@
 #The command line to valiate and create stack
 aws cloudformation validate-template --template-body file://auto-ec2-user-data.json
-aws cloudformation create-stack --stack-name my-ec2-user-data-stack --template-body file://my-auto-ec2-user-data.json --parameters ParameterKey=KeyName,ParameterValue=haiwenj-aws-us ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=SSHLocation,ParameterValue=180.166.53.0/24
+aws cloudformation create-stack --stack-name my-ec2-user-data-stack --template-body file://auto-ec2-user-data.json --parameters ParameterKey=KeyName,ParameterValue=haiwenj-aws-us ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=SSHLocation,ParameterValue=180.166.53.0/24
 
